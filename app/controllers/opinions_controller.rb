84ab57ae9ff4c7b952d60ddff14e2715a596fe26
sase_daiki <sase_daiki@yahoo.co.jp>
2020-01-29 13:14:07 +0000

    2020/01/29-1

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,5 +1,18 @@
 class OpinionsController < ApplicationController
   def index
     @opinion = Opinion.all
+    @newopinion = Opinion.new
   end
+  def create
+    @newopinion = Opinion.new(opinion_params)
+    if @newopinion.save
+      redirect_to root_url
+    else
+      render "index"
+    end
+  end
+  private
+    def opinion_params
+      params.require(:opinion).permit(:title, :text)
+    end
 end
