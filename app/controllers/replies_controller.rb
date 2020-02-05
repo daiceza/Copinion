27dfb1aacebca99dedacb54dd5731da47313c48c
sase_daiki <sase_daiki@yahoo.co.jp>
2020-02-05 12:57:40 +0000

    2020/02/05-1

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -3,18 +3,22 @@ class RepliesController < ApplicationController
   end
   def index
     @opinion = Opinion.find(params[:opinion_id])
-    @replylist = Reply.all
+    @replylist = Reply.where(opinion_id:params[:opinion_id])
+    #@replylist = Reply.all
     @reply = Reply.new
   end
   def create
     @opinion = Opinion.find(params[:opinion_id])
-    @replylist = Reply.all
+    @replylist = Reply.where(opinion_id:params[:opinion_id])
     @reply = Reply.new(reply_params)
     if @reply.save
-      redirect_to root_url
+      render 'index'
     else
       render 'index'
     end
+  end
+  def edit
+     @reply = Reply.find(params[:id])
   end
   private
     def reply_params
